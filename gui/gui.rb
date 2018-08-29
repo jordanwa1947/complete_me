a2611dcb2fc15c4a134116562626173ed82e5f2d
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-08-29 11:55:52 -0600

    updated gui, changed font size

--- gui/gui.rb
+++ gui/gui.rb
@@ -29,17 +29,23 @@ def definition(instance)
   instance.subpane("word_meaning")
 end
 
+def selected_word(instance)
+  instance.subpane("word_selection")
+end
+
 f.puts"Welcome to Data Finder!"
 f.html("<center> <h1> Welcome to Data Finder! </h1> </center>")
 
 input_chars = f.input("Type Something!")
 
-f.button("Find!!!!") do
+f.button("Find!") do
   suggestion(f).close
-  suggestion(f).html("<center><h2>pick a word!</h2><center>")
+  suggestion(f).html("<center><h2>Pick a word!</h2><center>")
   suggestion(f).dropdown(completion.suggest(input_chars)) do |word|
     completion.select(input_chars, word["value"])
-  definition(f).html("<center><h2>I'm a #{word["value"]} definition!</h2><center>")
+  word_definition = completion.fetch_definition(word["value"])
+  selected_word(f).html("<center><h2> You selected #{word["value"]}!</h2><center>")
+  definition(f).html("<center><h4>#{word_definition}</h4><center>")
   end
 end
 
